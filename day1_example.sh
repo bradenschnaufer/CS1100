23ea4cb97e84c00b3a151363ed0803d2db4c961c
student <braden.schnaufer@atlasschool.com>
2024-09-04 17:19:17 +0000

    first day of code. Totally confused

---
+++ day1_example.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#code is supposed to search for one of files
+#remove a file 
+#add a new directory to root named week_2
+
+find example.txt #finds file example.txt 
+ls #lists files in directory
+rm file1.sh #removes file1.sh
+ls #lists files in directory
+cd .. #backs into root
+mkdir Week_2 #makes week_2 directory
+cd Week_2
+ls
